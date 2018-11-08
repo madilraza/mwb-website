5e324713f1c0e426994d4d0b23c1d30a7f10f70a
Andrey Kolybelnikov <a.kolybelnikov@facevalue-global.com>
2018-11-08 14:06:43 +0100

    Removed block-end margin on figure element for proper alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarItem,
-  NavbarBurger,
-  NavbarMenu,
-  NavbarEnd
-} from 'bloomer'
+import { Navbar, NavbarBrand, NavbarItem, NavbarBurger, NavbarMenu, NavbarEnd } from 'bloomer'
 import logo from '../../static/logos/logo.png'
 
-const StyledLogo = styled.figure `
+const StyledLogo = styled.figure`
+  margin-bottom: 0;
   width: 60px;
   height: 60px;
   max-height: 100%;
@@ -29,98 +23,115 @@ const StyledLogo = styled.figure `
   }
 `
 
-  interface HeaderProps {
-    title : string
+interface HeaderProps {
+  title: string
+}
+
+interface State {
+  readonly isActive: boolean
+}
+
+export default class Header extends React.PureComponent<HeaderProps, State> {
+  readonly state: State = {
+    isActive: false
   }
 
-  interface State {
-    readonly isActive : boolean;
+  onClickNav = () => {
+    this.setState({
+      isActive: !this.state.isActive
+    })
   }
 
-  export default class Header extends React.PureComponent < HeaderProps,
-  State > {
-    readonly state : State = {
-      isActive: false
-    };
-
-    onClickNav = () => {
-      this.setState({
-        isActive: !this.state.isActive
-      })
-    }
-
-    render() {
-      const {title} = this.props
-      return (
-        <React.Fragment>
-          <Navbar isTransparent>
-            <NavbarBrand>
-              <NavbarItem>
-                <StyledLogo className="image is-square">
-                  <img src={logo}></img>
-                </StyledLogo>
-              </NavbarItem>
-              <NavbarItem>
+  render() {
+    const { title } = this.props
+    return (
+      <React.Fragment>
+        <Navbar isTransparent>
+          <NavbarBrand>
+            <NavbarItem>
+              <StyledLogo className="image is-square">
+                <img src={logo} />
+              </StyledLogo>
+            </NavbarItem>
+            <NavbarItem>
+              <Link className="title is-size-6-mobile is-size-5-tablet is-size-4-desktop" to="/">
+                {title}
+              </Link>
+            </NavbarItem>
+            <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+          </NavbarBrand>
+          <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
+            <NavbarEnd>
+              <NavbarItem hasTextAlign="centered">
                 <Link
-                  className="title is-size-6-mobile is-size-5-tablet is-size-4-desktop"
-                  to="/">{title}</Link>
+                  activeStyle={{
+                    color: '#fe5823'
+                  }}
+                  className="is-size-5 is-size-6-mobile"
+                  to="/about-us"
+                >
+                  About Us
+                </Link>
               </NavbarItem>
-              <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav}/>
-            </NavbarBrand>
-            <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
-              <NavbarEnd>
-                <NavbarItem hasTextAlign="centered">
-                  <Link
-                    activeStyle={{
+              <NavbarItem hasTextAlign="centered">
+                <Link
+                  activeStyle={{
                     color: '#fe5823'
                   }}
-                    className="is-size-5 is-size-6-mobile"
-                    to="/about-us">About Us</Link>
-                </NavbarItem>
-                <NavbarItem hasTextAlign="centered">
-                  <Link
-                    activeStyle={{
+                  className="is-size-5 is-size-6-mobile"
+                  to="/get-motivated"
+                >
+                  Get Motivated!
+                </Link>
+              </NavbarItem>
+              <NavbarItem hasTextAlign="centered">
+                <Link
+                  activeStyle={{
                     color: '#fe5823'
                   }}
-                    className="is-size-5 is-size-6-mobile"
-                    to="/get-motivated">Get Motivated!</Link>
-                </NavbarItem>
-                <NavbarItem hasTextAlign="centered">
-                  <Link
-                    activeStyle={{
+                  className="is-size-5 is-size-6-mobile"
+                  to="/partners"
+                >
+                  Partners
+                </Link>
+              </NavbarItem>
+              <NavbarItem hasTextAlign="centered">
+                <Link
+                  activeStyle={{
                     color: '#fe5823'
                   }}
-                    className="is-size-5 is-size-6-mobile"
-                    to="/partners">Partners</Link>
-                </NavbarItem>
-                <NavbarItem hasTextAlign="centered">
-                  <Link
-                    activeStyle={{
+                  className="is-size-5 is-size-6-mobile"
+                  to="/programming"
+                >
+                  Programming
+                </Link>
+              </NavbarItem>
+              <NavbarItem hasTextAlign="centered">
+                <Link
+                  activeStyle={{
                     color: '#fe5823'
                   }}
-                    className="is-size-5 is-size-6-mobile"
-                    to="/programming">Programming</Link>
-                </NavbarItem>
-                <NavbarItem hasTextAlign="centered">
-                  <Link
-                    activeStyle={{
+                  className="is-size-5 is-size-6-mobile"
+                  to="/apply-now"
+                >
+                  Apply Now
+                </Link>
+              </NavbarItem>
+              <NavbarItem hasTextAlign="centered">
+                <Link
+                  activeStyle={{
                     color: '#fe5823'
                   }}
-                    className="is-size-5 is-size-6-mobile"
-                    to="/apply-now">Apply Now</Link>
-                </NavbarItem>
-                <NavbarItem hasTextAlign="centered">
-                  <Link
-                    activeStyle={{
-                    color: '#fe5823'
-                  }}
-                    className="is-size-5 is-size-6-mobile"
-                    to="/contact">Contact</Link>
-                </NavbarItem>
-              </NavbarEnd>
-            </NavbarMenu>
-          </Navbar>
-        </React.Fragment>
-      )
-    }
+                  className="is-size-5 is-size-6-mobile"
+                  to="/contact"
+                >
+                  Contact
+                </Link>
+              </NavbarItem>
+            </NavbarEnd>
+          </NavbarMenu>
+        </Navbar>
+      </React.Fragment>
+    )
   }
+}
